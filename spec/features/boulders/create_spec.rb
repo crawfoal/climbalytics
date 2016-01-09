c1dc63ccc93e69b7dd45e78de4ee5fc854c92bbb
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-01-09 15:35:30 -0800

    link Create a... boulder problem to Boulders#create

---
+++ spec/features/boulders/create_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User creates a boulder problem' do
+  context 'when user has role of setter' do
+    before :each do
+      define_roles(:setter)
+      @user = create(:setter_user)
+      visit new_user_session_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_on 'Log in'
+    end
+    scenario 'from setter dashboard' do
+      within(".panel.create") do
+        click_on 'boulder problem'
+      end
+      expect(page).to have_content 'New Boulder'
+      expect(page).to have_button 'Create Boulder'
+    end
+  end
+end

472d6e815d828031b12a17f2c4636e7152935738
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-01-17 14:20:55 -0800

    For the setter_story spec, use `let` to create record instead of instance variable.

--- spec/models/setter_story_spec.rb
+++ spec/models/setter_story_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SetterStory, type: :model do
-  before :each do
-    @user = create(:setter_user)
-  end
+  let(:user) { create(:setter_user) }
+  
   it 'is created for a user after a role of setter is added' do
-    expect(@user.setter_story).to be_persisted
+    expect(user.setter_story).to be_persisted
   end
   it 'can create an associated boulder' do
-    boulder = @user.setter_story.boulders.create
-    expect(@user.setter_story.boulders.count).to be >= 1
+    boulder = user.setter_story.boulders.create
+    expect(user.setter_story.boulders.count).to be >= 1
     expect(boulder).to be_persisted
   end
 end

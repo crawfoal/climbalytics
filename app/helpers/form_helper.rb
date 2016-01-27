566d1f796a0d84042a91a85c9634cdeb0da9c622
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-01-26 16:56:33 -0800

    Add sign-in and sign-up forms to homepage
    Simply render devise's pages. Needed to define helper methods for `resource`, `resource_name` and `devise_mapping` which are defined in the `DeviseController` as protected methods and are not available outside of controllers that inherit from devise (and their corresponding views).

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,4 +4,14 @@ module FormHelper
     user.address ||= Address.new
     user
   end
+
+  def resource_name
+    :user
+  end
+  def resource
+    @resource ||= User.new
+  end
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end

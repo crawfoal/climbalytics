6eef8dea7c0c2d2ef6cb92f6a2ffa87b745d793f
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-02-01 22:01:18 -0800

    Refine athlete log association with boulder/route
    I made a module that you can use to define a type of association where the child is an STI model, and the parent should have only one of the STI types (e.g. either boulder or route). This is used in the parent model, after the `has_one` association declaration, and defines methods for the STI types that you can use to access and build the associated records.

--- spec/models/athlete_climb_log_spec.rb
+++ spec/models/athlete_climb_log_spec.rb
@@ -23,6 +23,73 @@ RSpec.describe AthleteClimbLog, type: :model do
         expect(athlete_climb_log.athlete).to be == user
       end
     end
+
+    describe 'climb associations' do
+      [:boulder, :route].each do |climb_type|
+        describe ".create_#{climb_type}" do
+          let(:climb) { athlete_climb_log.send("create_#{climb_type}", attributes_for(climb_type)) }
+          it "creates a #{climb_type}" do
+            expect(climb.type).to eq climb_type.to_s.capitalize
+          end
+          it 'persists the climb record' do
+            expect(climb).to be_persisted
+          end
+          it "sets the attributes for the #{climb_type}" do
+            attributes_for(climb_type).each do |attribute, value|
+              expect(climb.send(attribute)).to eq value
+            end
+          end
+          it 'associates the new boulder with the athlete climb log' do
+            climb
+            expect(athlete_climb_log.send(climb_type)).to be climb
+          end
+
+          it "calls #{climb_type.to_s.classify}.create with attributes" do
+            expect(climb_type.to_s.classify.constantize).to receive(:create).with(attributes_for(climb_type))
+            climb
+          end
+          it "calls AthleteClimbLog.climb=" do
+            expect(athlete_climb_log).to receive(:climb=)
+            climb
+          end
+        end
+
+        describe ".#{climb_type}" do
+          let(:climb) { athlete_climb_log.send("create_#{climb_type}", attributes_for(climb_type)) }
+          it "returns the climb if the climb's type is #{climb_type}" do
+            climb
+            expect(athlete_climb_log.send(climb_type)).to be climb
+          end
+          it "returns nil if the climb's type is not #{climb_type}" do
+            climb
+            expect(athlete_climb_log.send(climb_type == :route ? :boulder : :route)).to be_nil
+          end
+        end
+
+        describe ".#{climb_type}=" do
+          let(:climb) { climb_type.to_s.classify.constantize.new(attributes_for(climb_type)) }
+          before :each do
+            athlete_climb_log.send("#{climb_type}=", climb)
+          end
+
+          it "sets the climb to the record provided" do
+            expect(athlete_climb_log.climb).to be climb
+          end
+        end
+
+        describe ".create_#{climb_type}!" do
+          let(:climb) { athlete_climb_log.send("create_#{climb_type}!", attributes_for(climb_type)) }
+          it "calls #{climb_type.to_s.classify}.create! with attributes" do
+            expect(climb_type.to_s.classify.constantize).to receive(:create!).with(attributes_for(climb_type))
+            climb
+          end
+          it "calls AthleteClimbLog.climb=" do
+            expect(athlete_climb_log).to receive(:climb=)
+            climb
+          end
+        end
+      end
+    end
   end
 
 end

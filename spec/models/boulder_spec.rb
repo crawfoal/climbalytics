59359933fda877f4549d957078a30122ca7fb360
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-02-04 14:36:08 -0800

    Better comments and error messages for grade enums
    closes #42
    
    I decided not to explicitly write out the IDs. If I do it with a loop, it won't be much different than what I have now (in terms of the degree of "future-proofness"). I don't want to list everything out by hand because that is messy and error prone. I decided to just make it more obvious that something went wrong if the IDs are changed.

--- spec/models/boulder_spec.rb
+++ spec/models/boulder_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe Boulder, type: :model do
     # if you add to the Boulder.grades enum, make sure to maintain the ids below
     route_grades = Boulder.grades
     {"VB"=>0, "V0"=>1, "V1"=>2, "V2"=>3, "V3"=>4, "V4"=>5, "V5"=>6, "V6"=>7, "V7"=>8, "V8"=>9, "V9"=>10, "V10"=>11, "V11"=>12, "V12"=>13, "V13"=>14, "V14"=>15, "V15"=>16, "V16"=>17}.each do |key, value|
-      expect(route_grades[key]).to eq value
+      expect(route_grades[key]).to eq(value), %q(
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+The ID for one or more Route grades was modified. This change will modify the grade that the end users see on their logs. E.g. if the ID of V5 is changed from 6 to 7, and the ID of V6 is changed from 7 to 8, and so on, any log that had a grade of V6 and above will now show one grade lower than what the user had origianlly entered. Do not make this change!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      )
     end
   end
 end
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe Route, type: :model do
     # if you add to the Route.grades enum, make sure to maintain the ids below
     route_grades = Route.grades
     {"5.4"=>0, "5.5"=>1, "5.6"=>2, "5.7"=>3, "5.8"=>4, "5.9"=>5, "5.10a"=>6, "5.10b"=>7, "5.10c"=>8, "5.10d"=>9, "5.11a"=>10, "5.11b"=>11, "5.11c"=>12, "5.11d"=>13, "5.12a"=>14, "5.12b"=>15, "5.12c"=>16, "5.12d"=>17, "5.13a"=>18, "5.13b"=>19, "5.13c"=>20, "5.13d"=>21, "5.14a"=>22, "5.14b"=>23, "5.14c"=>24, "5.14d"=>25, "5.15a"=>26, "5.15b"=>27, "5.15c"=>28, "5.15d"=>29}.each do |key, value|
-      expect(route_grades[key]).to eq value
+      expect(route_grades[key]).to eq(value), %q(
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+The ID for one or more Route grades was modified. This change will modify the grade that the end users see on their logs. E.g. if the ID of 5.10a is changed from 6 to 7, and the ID of 5.10b is changed from 7 to 8, and so on, any log that had a grade of 5.10b and above will now show one grade lower than what the user had origianlly entered. Do not make this change!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      )
     end
   end
 end

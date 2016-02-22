6a4884145ed2bfdcd6c48f57674b28a9cf144c85
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-02-21 23:28:23 -0800

    remove unused methods `count` and `count=`

--- lib/data_generators/base.rb
+++ lib/data_generators/base.rb
@@ -3,19 +3,6 @@
 class BaseGenerator
   attr_accessor :min, :max
 
-  def count=(arg)
-    if arg.respond_to? :min and arg.respond_to? :max
-      @min = arg.min
-      @max = arg.max
-    elsif arg.is_a? Integer and arg.respond_to? :to_i
-      @min = @max = arg.to_i
-    end
-  end
-
-  def count
-    @min..@max
-  end
-
   def generate_one
     FactoryGirl.create(factory_name, *options)
   end

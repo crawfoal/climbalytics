84d3abb858e4bf8132188b30eb426df406ad6c90
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-03-26 11:35:48 -0700

    try just retrying `:js` tagged specs

--- spec/support/configurations/selenium.rb
+++ spec/support/configurations/selenium.rb
@@ -9,6 +9,9 @@ require 'rspec/retry'
 
 RSpec.configure do |config|
   config.verbose_retry = true # show retry status in spec process
-  config.default_retry_count = 20
   config.exceptions_to_retry = [Net::ReadTimeout] # only retry if this exception was thrown
+
+  config.around :each, :js do |ex|
+    ex.run_with_retry retry: 4
+  end
 end

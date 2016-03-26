751d4e7688b842ced16f43be8ca2688aff78cc78
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-03-26 11:44:01 -0700

    try increasing timeout

--- spec/support/configurations/selenium.rb
+++ spec/support/configurations/selenium.rb
@@ -1,7 +1,7 @@
 Capybara.register_driver :selenium do |app|
   profile = Selenium::WebDriver::Firefox::Profile.new
   client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 15 # instead of default of 60
+  # client.timeout = 15 # instead of default of 60
   Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, http_client: client)
 end
 

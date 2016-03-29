dba6e014ce3611be945eac16bbdeb509831f6383
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-03-28 17:35:42 -0700

    we're not using the `:rake_helper` tag anymore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -99,6 +99,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.filter_run_excluding :rake_helper
 end

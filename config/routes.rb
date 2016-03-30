c7d5b0e00b8d5dd669b4faefb8888d083f0dec35
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-03-30 15:23:03 -0700

    organize routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
+  # most common routes
   devise_for :user_accounts
-  resources :gyms, except: :destroy
   root 'home#show', via: [:get]
-
-  resource :user, only: [:edit, :update]
-
-  resource :current_role, only: [:update]
-
-  resources :athlete_climb_logs, only: [:new, :edit, :create, :update]
+  resource :athlete_dashboard, only: [:show]
+  resource :setter_dashboard, only: [:show]
   post 'athlete_climb_logs/new', to: 'athlete_climb_logs#new', as: ''
 
+  # all other resources alphabetically
+  resources :athlete_climb_logs, only: [:new, :edit, :create, :update]
   resources :climbs, only: [:index]
-
-  resource :athlete_dashboard, only: [:show]
-
-  resource :setter_dashboard, only: [:show]
-
-  resource :nearby_gyms, only: [:show]
-
   resources :climb_pickers, only: [:show]
-
+  resource :current_role, only: [:update]
   resource :flash, only: [:show]
+  resources :gyms, except: :destroy
+  resource :nearby_gyms, only: [:show]
+  resource :user, only: [:edit, :update]
 end

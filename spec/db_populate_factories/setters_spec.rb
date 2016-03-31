fd773162f5ef5e6468e6ca7f421900b427fbb1f1
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-03-30 22:10:18 -0700

    prevent the 1 in 30 failure, don't create up to 30 records
    that factory uses `Faker::Number.between(0,30)` to determine how many setter_climb_logs to create... we need to stub that out

--- spec/db_populate_factories/setters_spec.rb
+++ spec/db_populate_factories/setters_spec.rb
@@ -5,6 +5,7 @@ describe 'db:populate' do
     subject(:setter) { create :_setter_ }
 
     it 'has some associated setter_climb_logs (between 0 and 30)' do
+      allow(Faker::Number).to receive(:between).and_return(1)
       expect(setter.setter_story.setter_climb_logs.size).to be > 0
     end
   end
